e39b57848f6265074687bc586dc972c3c3a13cd8
“pankajnaik882” <pankaj080519999@gmail.com>
2023-01-27 13:43:01 +0530

    Add : UC5-Compute

--- sortingArithmeticProblem.sh
+++ sortingArithmeticProblem.sh
@@ -2,6 +2,6 @@ echo "Welcome To the Sorting Arithmetic Problem"
 read -p "Enter a first number: " a
 read -p "Enter b first number: " b
 read -p "Enter c first number: " c
-calculate=$(( $c + $a / $c))
-echo "calculate c + a / c :-" $calculate
+calculate=$(( $a % $b + $c ))
+echo "calculate a % b + c :-" $calculate
 
