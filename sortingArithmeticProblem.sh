7e4713aafab4aaf109424b66b4b5a1debd0a64eb
“pankajnaik882” <pankaj080519999@gmail.com>
2023-01-27 13:38:46 +0530

    Add : UC3-Compute

--- sortingArithmeticProblem.sh
+++ sortingArithmeticProblem.sh
@@ -2,6 +2,6 @@ echo "Welcome To the Sorting Arithmetic Problem"
 read -p "Enter a first number: " a
 read -p "Enter b first number: " b
 read -p "Enter c first number: " c
-calculate=$(( $a + $b * $c))
-echo "calculate a + b * c:-"$calculate
+calculate=$(( $a * $b + $c))
+echo "calculate a * b + c:-"$calculate
 
