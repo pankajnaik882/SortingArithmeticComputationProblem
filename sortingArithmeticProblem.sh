f3eb54357c1f45e0c848db73ad2a17999e80b032
“pankajnaik882” <pankaj080519999@gmail.com>
2023-01-27 13:46:59 +0530

    Add : Store the results in a Dictionary for every Computation

--- sortingArithmeticProblem.sh
+++ sortingArithmeticProblem.sh
@@ -1,7 +1,19 @@
-echo "Welcome To the Sorting Arithmetic Problem"
+echo "Welcome to the Sorting Arithmetic"
 read -p "Enter a first number: " a
 read -p "Enter b first number: " b
 read -p "Enter c first number: " c
-calculate=$(( $a % $b + $c ))
-echo "calculate a % b + c :-" $calculate
+
+temp1=$(($a + $b * $c))
+temp2=$((a * b + c))
+temp3=$((c + a / b))
+temp4=$((a % b + c))
+
+declare -A value
+
+value[1]="$temp1"
+value[2]="$temp2"
+value[3]="$temp3"
+value[4]="$temp4"
+
+
 
