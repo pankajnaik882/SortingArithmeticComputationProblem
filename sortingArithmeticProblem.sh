036ccbba6502936812245bec50c70cba359689a8
“pankajnaik882” <pankaj080519999@gmail.com>
2023-01-27 13:14:36 +0530

    Welcome to Sorting Arithmetic Computation Problem

---
+++ sortingArithmeticProblem.sh
@@ -0,0 +1,7 @@
+echo "Welcome To the Sorting Arithmetic Problem"
+read -p "Enter a first number: " a
+read -p "Enter b first number: " b
+read -p "Enter c first number: " c
+echo "First Value : "$a
+echo "Second Value : "$b
+echo "Third Value : "$c
